70ecfdfa1a75e87c83d265e96edbb1237cdc8b5e
Eduard-Kolchin <EduardKolchin@yandex.ru>
2023-11-11 17:37:07 +1000

    changing the style

--- task3/script.js
+++ task3/script.js
@@ -1,26 +1,31 @@
-const trafficLightEl = document.querySelector('#trafficLight');
-
-function makeGreen() {
-    trafficLightEl.style.background = ('green');
-    trafficLightEl.removeEventListener('click', makeGreen);
-    trafficLightEl.addEventListener('click', makeYellow);
-}
-
-trafficLightEl.addEventListener('click', makeGreen);
+const trafficLight = document.querySelector('#trafficLight');
+const LightE1 = document.querySelector('#Light1');
+const LightE2 = document.querySelector('#Light2');
+const LightE3 = document.querySelector('#Light3');
 
 function makeYellow() {
-    trafficLightEl.style.background = ('yellow');
-    trafficLightEl.removeEventListener('click', makeYellow);
-    trafficLightEl.addEventListener('click', makeRed);
-}
-
-trafficLightEl.addEventListener('click', makeYellow);
+    LightE1.style.background = ('black');
+    LightE2.style.background = ('yellow');
+    LightE3.style.background = ('black');
+    trafficLight.removeEventListener('click', makeYellow);
+    trafficLight.addEventListener('click', makeRed);
+};
+trafficLight.addEventListener('click', makeYellow);
 
 function makeRed() {
-    trafficLightEl.style.background = ('red');
-    trafficLightEl.removeEventListener('click', makeRed);
-    trafficLightEl.addEventListener('click', makeGreen);
-}
+    LightE1.style.background = ('black');
+    LightE2.style.background = ('black');
+    LightE3.style.background = ('red');
+    trafficLight.removeEventListener('click', makeRed);
+    trafficLight.addEventListener('click', makeGreen);
+};
+trafficLight.addEventListener('click', makeRed);
 
-trafficLightEl.addEventListener('click', makeRed);
-
+function makeGreen() {
+    LightE1.style.background = ('green');
+    LightE2.style.background = ('black');
+    LightE3.style.background = ('black');
+    trafficLight.removeEventListener('click', makeGreen);
+    trafficLight.addEventListener('click', makeYellow);
+};
+trafficLight.addEventListener('click', makeGreen);
